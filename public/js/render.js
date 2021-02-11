072b0651e79a34cee5292004b76bbd8bc75e3bb6
chenz16 <chenz16@rose-hulman.edu>
2021-02-10 19:23:52 -0600

    fix rushing subscription issue

--- public/js/render.js
+++ public/js/render.js
@@ -3,9 +3,11 @@ const color_g = 207;
 const color_b = 209;
 const light_black = "#05080d";
 
-const {webFrame} = require('electron');
+const { webFrame } = require('electron');
+const { watchFile } = require('fs');
+const { waitForDebugger } = require('inspector');
 
-webFrame.setZoomFactor(0.8);
+webFrame.setZoomFactor(1.0);
 
 function loadNewWindow() {
   console.log("open new window");
@@ -18,7 +20,7 @@ function loadNewWindow() {
       enableRemoteModule: true,
     }
   });
-  
+
   remote_window.loadURL(`file://${__dirname}/armcontrol.html`);
 }
 
@@ -35,7 +37,7 @@ function create_globe(div_id) {
   });
 }
 
-function updateScroll(div_id){
+function updateScroll(div_id) {
   var element = document.getElementById(div_id);
   element.scrollTop = element.scrollHeight;
 }
@@ -57,30 +59,57 @@ let streaming = {
 
 function subscribeCameraTopic(camera_id, camera_name, brand) {
   let video = document.getElementById(camera_id);
+  console.log(camera_name, streaming[camera_id]);
   if (streaming[camera_id] == false) {
-      streaming[camera_id] = true;
-      video.innerHTML = `<img id="${camera_name}" class="usb_cam">`;
-      video.style.marginTop = 0;
-      video.style.marginLeft = 0;
-      camera_info_topic.subscribe(function(message) {
-          document.getElementById(camera_name).src = "data:image/jpg;base64," + message.data;
-      });
-      log_status(CAMERA_SUB_ON, "status");
+    streaming[camera_id] = true;
+    video.innerHTML = `<img id="${camera_name}" class="usb_cam">`;
+    video.style.marginTop = 0;
+    video.style.marginLeft = 0;
+    console.log(document.getElementById(camera_name));
+    camera_info_topic.subscribe(function (message) {
+      document.getElementById(camera_name).src = "data:image/jpg;base64," + message.data;
+    });
+    log_status(CAMERA_SUB_ON, "status");
   } else if (streaming[camera_id] == true) {
-      streaming[camera_id] = false;
-      camera_info_topic.unsubscribe();
-      log_status(CAMERA_SUB_OFF, "status");
-      video.innerHTML =  `<h1>${brand}</h1>`;
-      video.style.marginTop = "auto";
-      video.style.marginLeft = "auto";
-      create_globe('#' + camera_id);
+    streaming[camera_id] = false;
+    camera_info_topic.unsubscribe();
+    log_status(CAMERA_SUB_OFF, "status");
+    video.innerHTML = `<h1>${brand}</h1>`;
+    video.style.marginTop = "auto";
+    video.style.marginLeft = "auto";
+    create_globe('#' + camera_id);
   }
 }
 
-window.audioManager = new AudioManager();
-window.audioManager.stdout.play();
-window.audioManager.theme.play();
+function subscribeCameraTopicArm(camera_id, camera_name, brand) {
+  let video = document.getElementById(camera_id);
+  console.log(camera_name, streaming[camera_id]);
+  if (streaming[camera_id] == false) {
+    streaming[camera_id] = true;
+    video.innerHTML = `<img id="${camera_name}" class="usb_cam">`;
+    video.style.marginTop = 0;
+    video.style.marginLeft = 0;
+    console.log(document.getElementById(camera_name));
+    arm_camera_info_topic.subscribe(function (message) {
+      document.getElementById(camera_name).src = "data:image/jpg;base64," + message.data;
+    });
+    log_status(CAMERA_SUB_ON, "status");
+  } else if (streaming[camera_id] == true) {
+    streaming[camera_id] = false;
+    arm_camera_info_topic.unsubscribe();
+    log_status(CAMERA_SUB_OFF, "status");
+    video.innerHTML = `<h1>${brand}</h1>`;
+    video.style.marginTop = "auto";
+    video.style.marginLeft = "auto";
+    create_globe('#' + camera_id);
+  }
+}
 
+//window.audioManager = new AudioManager();
+//window.audioManager.stdout.play();
+//window.audioManager.theme.play();
+
+/*
 document.querySelectorAll(".mod_column").forEach(e => {
   e.setAttribute("class", "mod_column activated");
 });
@@ -89,16 +118,17 @@ let i = 0;
 let left = document.querySelectorAll("#mod_column_left > div");
 let right = document.querySelectorAll("#mod_column_right > div");
 let x = setInterval(() => {
-    if (!left[i] && !right[i]) {
-        clearInterval(x);
-    } else {
-        window.audioManager.panels.play();
-        if (left[i]) {
-            left[i].setAttribute("style", "animation-play-state: running;");
-        }
-        if (right[i]) {
-            right[i].setAttribute("style", "animation-play-state: running;");
-        }
-        i++;
+  if (!left[i] && !right[i]) {
+    clearInterval(x);
+  } else {
+    window.audioManager.panels.play();
+    if (left[i]) {
+      left[i].setAttribute("style", "animation-play-state: running;");
     }
+    if (right[i]) {
+      right[i].setAttribute("style", "animation-play-state: running;");
+    }
+    i++;
+  }
 }, 500);
+*/
--- public/js/render_drive.js
+++ public/js/render_drive.js
@@ -1,21 +1,21 @@
 create_globe('#drive_video_wrapper');
 create_globe('#arm_video_wrapper');
 
-$("#driveviz").on("mouseover mouseout", function (e){
+$("#driveviz").on("mouseover mouseout", function (e) {
   videoPlayButtonControl("#drive_camera_control", e);
 });
 
-$("#drive_camera_play").on("click", function(e) {
+$("#drive_camera_play").on("click", function (e) {
   subscribeCameraTopic("drive_video_wrapper", "drive_cam", "WELCOME BACK CHRIS");
 });
 
 
-$("#armviz").on("mouseover mouseout", function (e){
+$("#armviz").on("mouseover mouseout", function (e) {
   videoPlayButtonControl("#arm_camera_control", e);
 });
 
-$("#arm_camera_play").on("click", function(e) {
-  subscribeCameraTopic("arm_video_wrapper", "arm_cam", "RHIT ROVER TEAM");
+$("#arm_camera_play").on("click", function (e) {
+  subscribeCameraTopicArm("arm_video_wrapper", "arm_cam", "RHIT ROVER TEAM");
 });
 
 
